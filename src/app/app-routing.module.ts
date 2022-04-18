e292e0e20b5d1104cf01d3a3994fb8ac8ab617fd
salar.kazazi@gmail.com <salar.kazazi@gmail.com>
2022-04-18 05:35:18 +0430

    fixed nav zindex

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,12 +8,16 @@ const routes: Routes = [
     path: '',
     component: HomeComponent,
     pathMatch: 'full'
-  } ,
+  },
   {
     path: 'paperwall',
     component: PaperwallComponent,
     pathMatch: 'full'
-  } 
+  },
+  {
+    path: "**",
+    redirectTo: ''
+  }
 ];
 
 @NgModule({
